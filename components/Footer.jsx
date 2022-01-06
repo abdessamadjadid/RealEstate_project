f486bc892867980d51a9a147bdba215ab047ffa5
Abdessamad-Jadid <ajadid@miu.edu>
2022-01-06 00:17:01 -0600

    Add Footer Component

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import {Box} from '@chakra-ui/react'
+
+const footer = () => 
+(
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
+        2021 Realtor , Inc
+
+    </Box>
+)
+
+export default footer;
