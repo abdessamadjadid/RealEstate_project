ccca0091182bcc0ea61e2055519b48cb563fb9b2
Abdessamad-Jadid <ajadid@miu.edu>
2022-01-05 22:13:28 -0600

    import Navbar to Layout file

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import {Box} from '@chakra-ui/react'
-import { Children } from 'react/cjs/react.production.min';
+import {children} from 'react/cjs/react.production.min';
+
+import Navbar from './Navbar';
 
 const Layout = ({children}) => (
     <>
@@ -9,7 +11,7 @@ const Layout = ({children}) => (
         </Head>
         <Box maxWidth="1280px" m="auto">
             <header>
-                Navbar
+                <Navbar/>
             </header>
 
             <main>
