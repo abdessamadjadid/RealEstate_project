6a4c1aae118d57474a9c59029cde0e24e28a9f7b
Abdessamad-Jadid <ajadid@miu.edu>
2022-01-06 23:59:20 -0600

    Add ImageScrollbar component & add [id].js page to pass the id to property page

---
+++ pages/property/[id].js
@@ -0,0 +1,31 @@
+import {Box,Flex,Spacer,Text,Avatar} from '@chakra-ui/react';
+import {FaBed,FaBath} from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import {GoVerified} from 'react-icons/go';
+import millify from "millify";
+
+import { baseUrl,fetchApi } from '../../utils/fetchApi';
+import Property from '../../components/property';
+import ImageScrollbar from '../../components/ImageScrollbar';
+
+const PropertyDetails = ({PropertyDetails : {price,rentFrequency,rooms,title,baths,area,agency,isVerified,description,type,purpose,furnishingStatus,amenities,photos}}) =>
+(
+    <Box maxWidth="1000px" margin="auto" p="4">
+        {photos && <ImageScrollbar data={photos} />}
+    </Box>
+);
+
+export default PropertyDetails;
+
+
+
+export async function getServerSideProps({params:{id}}){
+
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+return{
+    props:{
+        PropertyDetails: data
+    }
+}
+
+}
